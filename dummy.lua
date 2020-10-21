eb14526f991631f589419b0cd8dfe8a52379382f
dist-ian <71764999+dist-ian@users.noreply.github.com>
2020-10-21 12:45:47 +0700

    Update dummy.lua

--- dummy.lua
+++ dummy.lua
@@ -24,35 +24,36 @@ if Runtime.getVersionCode() >= 1937 then
     )
   end
 end
-  -- dialog to select hidden draft
-  -- the draft only can be selected with this tool
-  local function showDraftDialog()
-    local dialog = GUI.createDialog{
-      w = 256,
-      h = 256,
-      title = 'Select Building Size'
-    }
-    local listBox = dialog.content:addListBox{}
-    for i=1, 8 do
-      local layout = listBox:addLayout{h=26,spacing=1}
-      local label = layout:getFirstPart():addLabel{
-        w = -26,
-        text = Draft.getDraft('$dummy'..i):getTitle()
-      }
-      local button = layout:getLastPart():addButton{
-        w = 0,
-        icon = Icon.BUILD,
-        onClick = function()
-          City.createDraftDrawer('$dummy'..i).select()
-          dialog.close()
-        end
-      }
-    end
-  end
 
-  -- function to close the build mode and show the dialog
-  -- another function to prevent the dialog show
-  -- when the draft has been built
+-- dialog to select hidden draft
+-- the draft only can be selected with this tool
+local function showDraftDialog()
+  local dialog = GUI.createDialog{
+    w = 256,
+    h = 256,
+    title = 'Select Building Size'
+  }
+  local listBox = dialog.content:addListBox{}
+  for i=1, 8 do
+    local layout = listBox:addLayout{h=26,spacing=1}
+    local label = layout:getFirstPart():addLabel{
+      w = -26,
+      text = Draft.getDraft('$dummy'..i):getTitle()
+    }
+    local button = layout:getLastPart():addButton{
+      w = 0,
+      icon = Icon.BUILD,
+      onClick = function()
+        City.createDraftDrawer('$dummy'..i).select()
+        dialog.close()
+      end
+    }
+  end
+end
+
+-- function to close the build mode and show the dialog
+-- another function to prevent the dialog show
+-- when the draft has been built
 function script:event(x,y,level,event)
   if event == Script.EVENT_TOOL_ENTER then
     GUI.get'cmdCloseTool':click()
